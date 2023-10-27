95ceee6203feccc3a6ba6eeb3d506c0e0339e09a
Juby210 <31005896+Juby210@users.noreply.github.com>
2023-10-27 19:02:06 +0200

    rewrite nativelib to c

--- app/src/main/cpp/nativelib.c
+++ app/src/main/cpp/nativelib.c
@@ -1,9 +1,10 @@
 #include <jni.h>
-#include <string>
+#include <string.h>
+#include <stdbool.h>
 #include <dlfcn.h>
 #include "hook.h"
 
-static HookFunType hook_func = nullptr;
+static HookFunType hook_func = NULL;
 
 jint (*backup)(JavaVM *, void *);
 
@@ -11,19 +12,20 @@ jint fakeLoad(JavaVM *, void *) {
     return JNI_VERSION_1_6;
 }
 
-bool ends_with(const std::string& a, const std::string& b) {
-    if (b.size() > a.size()) return false;
-    return std::equal(a.begin() + a.size() - b.size(), a.end(), b.begin());
+bool ends_with(const char *a, const char *b) {
+    size_t len = strlen(a);
+    size_t len2 = strlen(b);
+    if (len2 > len) return false;
+    return strncmp(a + len - len2, b, len2) == 0;
 }
 
 void on_library_loaded(const char *name, void *handle) {
-    if (ends_with(std::string(name), "libnative-lib.so")) {
+    if (ends_with(name, "libnative-lib.so")) {
         void *target = dlsym(handle, "JNI_OnLoad");
         hook_func(target, (void *) fakeLoad, (void **) &backup);
     }
 }
 
-extern "C" [[gnu::visibility("default")]] [[gnu::used]]
 NativeOnModuleLoaded native_init(const NativeAPIEntries *entries) {
     hook_func = entries->hook_func;
     return on_library_loaded;
