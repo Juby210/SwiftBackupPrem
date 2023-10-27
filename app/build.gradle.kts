ca275cc1ef476247f595c6c093deb891e8aab822
Juby210 <31005896+Juby210@users.noreply.github.com>
2023-10-27 19:01:18 +0200

    bump deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,13 @@ plugins {
 
 android {
     namespace = "io.github.juby210.swiftbackupprem"
-    compileSdk = 33
+    compileSdk = 34
     ndkVersion = "25.1.8937393"
 
     defaultConfig {
         applicationId = "io.github.juby210.swiftbackupprem"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 200
         versionName = "2.0.0"
     }
@@ -36,10 +36,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
     buildFeatures {
+        buildConfig = true
         compose = true
         resValues = false
     }
-    composeOptions.kotlinCompilerExtensionVersion = "1.3.1"
+    composeOptions.kotlinCompilerExtensionVersion = "1.5.3"
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             jvmTarget = "11"
@@ -51,16 +52,15 @@ dependencies {
     compileOnly("de.robv.android.xposed:api:82")
 
     // AndroidX
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.0")
 
     // Compose
-    val composeVersion = "1.3.0-beta02"
+    val composeVersion = "1.5.4"
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.0.1")
+    implementation("androidx.compose.material3:material3:1.1.2")
 
     // Accompanist
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.26.3-beta")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,23 @@
 buildscript {
     repositories {
         google()
+        //noinspection JcenterRepositoryObsolete
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
 allprojects {
     repositories {
         google()
+        //noinspection JcenterRepositoryObsolete
         jcenter()
     }
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
